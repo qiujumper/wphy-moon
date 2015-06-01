027cb9cc6f41a99969efb1791cddb53207b437be
jack.qiu <jack.qiu@frogdesign.com>
2015-06-01 18:11:25 +0800

    add body colour

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site container">
+<div id="page" class="hfeed site container-fluid">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wphy-sun' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
