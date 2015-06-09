877f7e1fac56f6df9824e18dbd37ee85ea959e13
jack.qiu <jack.qiu@frogdesign.com>
2015-06-09 12:26:53 +0800

    button area

--- module/button-text.php
+++ module/button-text.php
@@ -1,3 +1,9 @@
+<?php 
+if (ot_get_option('show_button_block')=='on') {
+?>
 <div class="button-text">
-  <span class="content">How are you doing today? </span><a href="#">contact us</a>
+  <span class="content"><?php echo ot_get_option('button_block_content'); ?> </span><a target="_blank" href="<?php echo ot_get_option('button_block_link'); ?>"><?php echo ot_get_option('button_block_title'); ?></a>
 </div>
+<?php 
+}
+?>
