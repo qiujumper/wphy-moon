2d5531f5a65ae186a1fe9c689763ab3ed2cfa97d
jack.qiu <jack.qiu@frogdesign.com>
2015-06-11 15:22:36 +0800

    fix page layout

--- page.php
+++ page.php
@@ -1,16 +1,8 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package wphy-sun
- */
-
 get_header(); 
+?>
+<div class="container">
+<?php
 get_sidebar();
 ?>
 
@@ -32,5 +24,5 @@ get_sidebar();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div><!-- container -->
 <?php get_footer(); ?>
