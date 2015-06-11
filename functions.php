77702b5d0a85e779fb804499dc1238a217cc1c01
jack.qiu <jack.qiu@frogdesign.com>
2015-06-11 18:29:57 +0800

    add the option tree, but I will remove it later

--- functions.php
+++ functions.php
@@ -6,6 +6,16 @@
  */
 require get_template_directory() . '/inc/helper.php';
 require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
+/**
+ * Required: set 'ot_theme_mode' filter to true.
+ */
+add_filter( 'ot_theme_mode', '__return_true' );
+
+/**
+ * Required: include OptionTree.
+ */
+require( trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php' );
+
 
 if ( ! function_exists( 'wphy_sun_setup' ) ) :
 /**
