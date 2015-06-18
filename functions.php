c88e89350dd7501a851fe33988dfaf66b541e557
jack.qiu <jack.qiu@frogdesign.com>
2015-06-18 17:13:08 +0800

    remove jetpack

--- functions.php
+++ functions.php
@@ -159,10 +159,6 @@ require get_template_directory() . '/inc/extras.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
 
 function wphy_comment($comment, $args, $depth){
 	$GLOBALS['comment'] = $comment;
