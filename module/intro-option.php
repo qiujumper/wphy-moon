8e2d39bafddaaaacfc2f5b7562d7426998db01d4
Jack Qiu <jack.qiu@frogdesign.com>
2015-07-13 17:37:45 +0800

    bugfix for intro blog

--- module/intro-option.php
+++ module/intro-option.php
@@ -1,5 +1,5 @@
 <div class=" option-block animated " style="background-color:rgba(<?php echo $data['color']; ?>,0.9);">
-  <a href="<?php echo $data['url']; ?>" href="_blank">
+  <a href="<?php echo $data['url']; ?>" target="_blank">
     <span class="icon"><?php echo $data['icon']; ?></span>
     <div class="title"><?php echo $data['title']; ?></div>
   </a>
