631f8a5c53eff4562cdbb0c03b3d1c94483bbc32
Jack Qiu <jack.qiu@frogdesign.com>
2015-07-14 13:05:46 +0800

    change the order of homepage section

--- front-page.php
+++ front-page.php
@@ -7,15 +7,15 @@ include(locate_template('module/slider.php'));
     <main id="main" class="site-main" role="main">
 
     <?php
-    include(locate_template('module/intro-page.php')); 
-    include(locate_template('module/icon-text-area.php'));
-    include(locate_template('module/half-block-area.php'));
-    include(locate_template('module/image-block-area.php'));
-    include(locate_template('module/full-width-text-block.php'));
-    include(locate_template('module/text-block-area.php'));
-    include(locate_template('module/image-post-slider.php'));
-    include(locate_template('module/button-text.php'));
-    include(locate_template('module/case-area.php'));
+        include(locate_template('module/intro-page.php')); 
+        include(locate_template('module/icon-text-area.php'));
+        include(locate_template('module/half-block-area.php'));
+        include(locate_template('module/image-block-area.php'));
+        include(locate_template('module/full-width-text-block.php'));
+        include(locate_template('module/text-block-area.php'));
+        include(locate_template('module/image-post-slider.php'));
+        include(locate_template('module/case-area.php'));
+        include(locate_template('module/button-text.php'));
 
     ?>
 
