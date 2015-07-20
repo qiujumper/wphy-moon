a409fbe651da14fa1e9317bc8d3eaf6ca6051e35
Jack Qiu <jack.qiu@frogdesign.com>
2015-07-20 19:17:50 +0800

    update the 404 page

--- 404.php
+++ 404.php
@@ -7,6 +7,12 @@
 
 get_header();
 ?>
+<header class="page-header">
+  <h1>Wordpress花园</h1>
+  <div class="taxonomy-description">
+		<?php echo get_option('about_us'); ?>  
+	</div>
+</header><!-- .page-header --> 
 <div class="container">
 <?php  
 get_sidebar();
@@ -17,44 +23,182 @@ get_sidebar();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( '抱歉,页面没找到.', 'wphy_moon' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( '抱歉,页面没找到:(', 'wphy_moon' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wphy_moon' ); ?></p>
+					<p><?php esc_html_e( '没找到就搜索一下吧:D' ); ?></p>
 
 					<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( wphy_moon_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( '热门分类', 'wphy_moon' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wphy_moon' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
+			<div class="all-list-area">
 
+      <div class="item-block">
+        <div class="title">
+          <h3>新手入门教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'simple',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+      <div class="item-block">
+        <div class="title">
+          <h3>插件使用教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'post',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+      <div class="item-block">
+        <div class="title">
+          <h3>模板制作教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'theme',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+      <div class="item-block">
+        <div class="title">
+          <h3>woocommce商城教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'woo',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+      <div class="item-block">
+        <div class="title">
+          <h3>插件制作教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'create',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+      <div class="item-block">
+        <div class="title">
+          <h3>高级技巧教程</h3>
+        </div>
+        <div class="item-group">
+          <div class="row">
+          <?php 
+            $args_simple = array(
+              'post_type' => 'advanced',
+              'posts_per_page' => -1,
+              'order' => 'ASC'
+            );
+            $query_simple = new WP_Query($args_simple);
+            if ( $query_simple->have_posts() ) : while ( $query_simple->have_posts() ) : $query_simple->the_post(); 
+          ?>
+            <div class="col-xs-6">
+              <div class="list-title">
+                <span class="glyphicon glyphicon-triangle-right"></span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div><!-- list-title -->
+            </div>  <!-- col-xs-6 -->
+            <?php
+            endwhile; endif;  
+            ?>
+          </div><!-- row -->
+        </div><!-- item-group -->
+      </div>  <!-- item-block -->
+
+    </div><!-- all-list-area -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </div><!-- container -->
